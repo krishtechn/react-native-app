8136df5cef1a4aed74899241cefa37b72f79b24f
krishtechn <krishkharal190@gmail.com>
2022-05-05 11:38:41 +0545

    react-native-online-course-app

--- App.js
+++ App.js
@@ -16,76 +16,75 @@ import ImagePicker from "./Components/ImagePicker";
 const App = () => {
   const stack = createNativeStackNavigator();
   return (
-    <View>
-      <ImagePicker />
-    </View>
-    // <NavigationContainer>
-    //   {/* <Tabbar /> */}
-    //   <StatusBar backgroundColor="rgb(33, 150, 243)"/>
-    //   {/* <Bottomsheet /> */}
-    //   <stack.Navigator initialRouteName='Login'>
+    // <View>
+    //   <ImagePicker />
+    // </View>
+     <NavigationContainer>
+       {/* <Tabbar /> */}
+       <StatusBar backgroundColor="rgb(33, 150, 243)"/>
+       {/* <Bottomsheet /> */}
+       <stack.Navigator initialRouteName='Login'>
 
-    //       <stack.Screen name="home">
-    //        {(props)=><Homescreen {...props} title="welcome"/>}  
-    //       </stack.Screen>  
+         <stack.Screen name="home">
+          {(props)=><Homescreen {...props} title="welcome"/>}  
+         </stack.Screen>  
 
-    //       <stack.Screen name="Login" component={Login}/>
+          <stack.Screen name="Login" component={Login}/>
 
-    //       <stack.Screen name="User" component={Users}/>
+           <stack.Screen name="User" component={Users}/>
 
-    //       <stack.Screen name="Course" component={Couses}/>
+           <stack.Screen name="Course" component={Couses}/>
 
-    //       <stack.Screen name="about">
-    //       {(props)=><About {...props} title="Technical krish" desc="Hello I am krish kharal full stack developer" url={image} />}  
-    //       </stack.Screen>
+           <stack.Screen name="about">
+           {(props)=><About {...props} title="Technical krish" desc="Hello I am krish kharal full stack developer" url={image} />}  
+           </stack.Screen>
 
-    //       <stack.Screen name="Gallery" component={Gallery}/>
+          <stack.Screen name="Gallery" component={Gallery}/>
 
           
-    //   </stack.Navigator>
+        </stack.Navigator>
 
-    // </NavigationContainer>
+     </NavigationContainer>
   )
 }
 
-// const Homescreen = ({title})=>{
-//   const navigation = useNavigation();
-//   return (
-//     <View style={{width:"100%",height:"100%",display:"flex",justifyContent:"space-between",flexDirection:"column"}}>
-//      <FlatList data={apidata} horizontal showsHorizontalScrollIndicator={false} style={{backgroundColor:"rgb(92, 108, 124)",}} renderItem={(items)=>{
-//        return <View style={{backgroundColor:"orange",height:"20%",padding:10,margin:10,display:"flex",justifyContent:"center",alignItems:"center"}}>
-//            <Text>{items.item.title}</Text>
-//            </View>
-//      }}/>
-//    <View style={{display:"flex",flexDirection:"row",justifyContent:"space-between",marginHorizontal:30}}>
-//    <Button title="About us" onPress={()=>{
-//         navigation.navigate("about");
-//       }}/>
+const Homescreen = ({title})=>{
+  const navigation = useNavigation();
+  return (
+    <View style={{width:"100%",height:"100%",display:"flex",justifyContent:"space-between",flexDirection:"column"}}>
+     <FlatList data={apidata} horizontal showsHorizontalScrollIndicator={false} style={{backgroundColor:"rgb(92, 108, 124)",}} renderItem={(items)=>{
+       return <View style={{backgroundColor:"orange",height:"20%",padding:10,margin:10,display:"flex",justifyContent:"center",alignItems:"center"}}>
+           <Text>{items.item.title}</Text>
+           </View>
+     }}/>
+   <View style={{display:"flex",flexDirection:"row",justifyContent:"space-between",marginHorizontal:30}}>
+   <Button title="About us" onPress={()=>{
+        navigation.navigate("about");
+      }}/>
 
-// <Button title="User Data" onPress={()=>{
-//         navigation.navigate("User");
-//       }}/>
-//  <Button title="Share" onPress={()=>{
-//     const result = Share.share({
-//       message:"Hello world",
-//     })
-//   //  Linking.openSettings();
-//   // Linking.openURL("https://www.youtube.com/results?search_query=react+native+bottom+sheet");
-//       }}/>
+ <Button title="User Data" onPress={()=>{
+         navigation.navigate("User");
+       }}/>
+  <Button title="Share" onPress={()=>{
+     const result = Share.share({
+       message:"Hello world",
+     })
+   //  Linking.openSettings();
+   // Linking.openURL("https://www.youtube.com/results?search_query=react+native+bottom+sheet");
+       }}/>
+ <Button title="Course" onPress={()=>{
+         navigation.navigate("Course");
+      }}/>
 
-// <Button title="Course" onPress={()=>{
-//         navigation.navigate("Course");
-//       }}/>
-
-// <Button title="Gallery" onPress={()=>{
-//         navigation.navigate("Gallery");
-//       }}/>
+ <Button title="Gallery" onPress={()=>{
+         navigation.navigate("Gallery");
+       }}/>
 
       
-//    </View>
-// </View>
-//   )
-// }
+    </View>
+ </View>
+   )
+ }
 
 
 
--- README.md
+++ README.md
@@ -1,13 +1,33 @@
-what we learn
-Liveseach api image
-Login
-Navigation
-react native paper for ui
-how to add splash screen 
- - to add splash screen 
-    Open up app.json from your project directory in your code editor and make the following change in the splash section:
-     "splash": {
-        "image": "./assets/splash.png",
-        "resizeMode": "contain",
-        "backgroundColor": "#000000"
+# React native App online Course app,about Screen login and signup. 
+React native tutorial design signup page, about page, userData page, share our portfolio online Courses, Live image search 
+
+# Our app demo image
+***App Demo:***<br><br>
+
+## what we learn in this tutorial
+  - react native Image Picker from Gallery<br>
+  - share button
+  - react native paper for ui<br>
+  - search Image app using unsplash api
+  - Linking = click and go from facebook,youtube<br>
+  - fetch data from api and show using useState Hooks<br>
+  - BottomSheet in react native<br>
+  - Tabbar in react native<br>
+  - SignUp form design <br>
+  - How to used Google font<br>
+  - Moving between one screen to another using react native Navigation<br>
+  - Loading <br>
+
+# how to add splash screen 
+ - to add splash screen<br>
+    Open up app.json from your project directory in your code editor and make the following change in the splash section:<br>
+     "splash": {<br>
+        "image": "./assets/splash.png",<br>
+        "resizeMode": "contain",<br>
+        "backgroundColor": "#000000"<br>
       },
+
+
+# Google font : 
+https://www.npmjs.com/package/@expo-google-fonts/inter
+
